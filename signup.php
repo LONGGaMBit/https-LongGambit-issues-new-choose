400a87e4da4103c2f2938eaddf75b6ba661f076d
LONGGaMBit <hoanglong83457@gmail.com>
2026-03-29 07:06:43 +0700

    Add signup functionality to insert user datae
    Implement user signup functionality with database insertion.

---
+++ signup.php
@@ -0,0 +1,16 @@
+<?php
+$conn = new mysqli("localhost","root","","users_db");
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (username,email,password)
+VALUES ('$username','$email','$password')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Đăng ký thành công";
+} else {
+    echo "Lỗi: " . $conn->error;
+}
+?>
